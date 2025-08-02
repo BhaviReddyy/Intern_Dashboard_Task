da5f39c9d3dd7e89870cc46250af690407b1c98b
BhaviReddyy <bhavyareddy506@gmail.com>
2025-08-02 21:51:08 +0530

    urls.py

--- urls.py
+++ urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from dashboard_module import views  # Import your app views
+from dashboard_module import views  # <-- Import your view here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.dashboard_view, name='dashboard'),  # Root URL to dashboard_view
+    path('', views.dashboard_view, name='dashboard'),  # <-- Add this line
 ]
-
